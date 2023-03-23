7ff7ab1dfdf1336d0372c38cf2e42f5df3ebffed
Linduri <tooth.ted@gmail.com>
2023-03-23 20:16:24 +0000

    Took load file out of function

--- src/main2.py
+++ src/main2.py
@@ -40,6 +40,7 @@ COLS = {
     "P_LAND_LATE": 6
 }
 
+
 def split_terms(line):
     """
     Split a line of numbers into a list of floats
@@ -47,63 +48,64 @@ def split_terms(line):
     nums = re.split('\s+', line.strip())
     return [float(num) for num in nums]
 
-def load_data(path):
-    """
-    Load and parse a .txt file to a numpy array
-    """
-    with open(path, 'r', encoding="utf-8") as file:
 
-        # Get the plane count and freeze time from the first line.
+with open(filepath, 'r', encoding="utf-8") as file:
+
+    # Get the plane count and freeze time from the first line.
+    next_line = file.readline()
+
+    terms = None if not next_line else split_terms(next_line)
+
+    n_planes = int(terms[0])
+    t_freeze = terms[1]
+    print(f"Plane count: {n_planes}")
+    print(f"Freeze time: {t_freeze}")
+
+    # Keep reading all remaining terms in groups of 6 + n_planes
+    # columns = plane attributes + n_planes separation
+    # rows = n_planes
+    var_per_plane = len(COLS) + n_planes
+    data = np.zeros([n_planes, var_per_plane])
+
+    term_idx = 0
+    plane_idx = 0
+    while True:
         next_line = file.readline()
+
+        # Exit if end of file
+        if not next_line:
+            break
 
         terms = None if not next_line else split_terms(next_line)
 
-        n_planes = int(terms[0])
-        t_freeze = terms[1]
-        print(f"Plane count: {n_planes}")
-        print(f"Freeze time: {t_freeze}")
+        for term in terms:
+            if term_idx == 0:
+                data[plane_idx, COLS["T_APPEAR"]] = term
+            elif term_idx == 1:
+                data[plane_idx, COLS["T_LAND_EARLY"]] = term
+            elif term_idx == 2:
+                data[plane_idx, COLS["T_LAND_TARGET"]] = term
+            elif term_idx == 3:
+                data[plane_idx, COLS["T_LAND_LATE"]] = term
+            elif term_idx == 4:
+                data[plane_idx, COLS["P_LAND_EARLY"]] = term
+            elif term_idx == 5:
+                data[plane_idx, COLS["P_LAND_LATE"]] = term
+            else:
+                if term_idx == 6:
+                    term_idx += 1
 
-        # Keep reading all remaining terms in groups of 6 + n_planes
-        # columns = plane attributes + n_planes separation
-        # rows = n_planes
-        var_per_plane = len(COLS) + n_planes
-        data = np.zeros([n_planes, var_per_plane])
-        
-        term_idx = 0
-        plane_idx = 0
-        while True:
-            next_line = file.readline()
+                data[plane_idx, term_idx] = term
 
-            #Exit if end of file
-            if not next_line:
-                break
+            term_idx += 1
+            if term_idx == var_per_plane:
+                plane_idx += 1
+                term_idx = 0
 
-            terms = None if not next_line else split_terms(next_line)
+data[:, COLS["T_LAND_ASSIGNED"]] = np.round(random.uniform(
+    data[:, COLS["T_LAND_EARLY"]], data[:, COLS["T_LAND_LATE"]]))
 
-            for term in terms:
-                if term_idx == 0:
-                    data[plane_idx, COLS["T_APPEAR"]] = term
-                elif term_idx == 1:
-                    data[plane_idx, COLS["T_LAND_EARLY"]] = term
-                elif term_idx == 2:
-                    data[plane_idx, COLS["T_LAND_TARGET"]] = term
-                elif term_idx == 3:
-                    data[plane_idx, COLS["T_LAND_LATE"]] = term
-                elif term_idx == 4:
-                    data[plane_idx, COLS["P_LAND_EARLY"]] = term
-                elif term_idx == 5:
-                    data[plane_idx, COLS["P_LAND_LATE"]] = term
-                else:
-                    if term_idx == 6:
-                        term_idx += 1
-                        
-                    data[plane_idx, term_idx] = term
-
-                term_idx += 1
-                if term_idx == var_per_plane:
-                    plane_idx += 1
-                    term_idx = 0
-    
-    return data
-
-print
+columns = ["t_appear", "t_land_early", "t_land_target", "t_land_assigned",
+           "t_land_late", "p_land_early", "p_land_late"] + [f"sep_{i}" for i in range(n_planes)]
+df = pd.DataFrame(data=data, columns=columns)
+print(df)
