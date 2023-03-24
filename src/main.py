518d4b0134e7cb9a797444f649fcf92360f18242
Linduri <tooth.ted@gmail.com>
2023-03-24 15:57:28 +0000

    Converted schedule drawing function to black and white for persons with colour recognition difficulties

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from pymoo.optimize import minimize
 from pymoo.visualization.scatter import Scatter
 from pymoo.core.callback import Callback
 from data_loader import load_data, COLS
-from PIL import Image
+from PIL import Image, ImageDraw
 
 import pathlib
 import matplotlib.pyplot as plt
@@ -24,27 +24,58 @@ n_planes, t_freeze, data, lower_bounds, upper_bounds = load_data(filepath)
 print(data)
 
 # ================================================== DRAW SCHEDULE
-def draw_planes(planes, pixel_height=10, gap_height=3):
+def draw_planes(planes, pixel_height=20, gap_height=3):
     _lower_bounds = planes.min(axis=0)
     _upper_bounds = planes.max(axis=0)
     width = int(_upper_bounds[COLS["T_LAND_EARLY"]] + _upper_bounds[COLS["T_LAND_LATE"]] - _lower_bounds[COLS["T_LAND_EARLY"]])
     row_height = pixel_height+gap_height
-    image = Image.new('RGB', (width, planes.shape[0]*row_height))    
+    
+    image = Image.new('RGB', (width, planes.shape[0]*row_height))
+    ImageDraw.floodfill(image, xy=(0,0), value=(255,255,255))
+
+    # for idx, plane in enumerate(planes):
+    #     land_window = int(plane[COLS["T_LAND_LATE"]] - plane[COLS["T_LAND_EARLY"]])
+    #     for i in range(land_window):
+    #         for j in range(pixel_height): 
+    #             image.putpixel((int(plane[COLS["T_LAND_EARLY"]]) + i - 1, (idx*row_height)+j), (0,180,0))
+
+    #     for j in range(pixel_height):
+    #         image.putpixel((int(plane[COLS["T_LAND_ASSIGNED"]])-1, (idx*row_height)+j), (255,255,255))
+
+    #     for j in range(pixel_height):
+    #         image.putpixel((int(plane[COLS["T_APPEAR"]])-1, (idx*row_height)+j), (255,100,100))
+
+    #     for j in range(pixel_height):
+    #         image.putpixel((int(plane[COLS["T_LAND_TARGET"]])-1, (idx*row_height)+j), (255,255,128))
 
     for idx, plane in enumerate(planes):
         land_window = int(plane[COLS["T_LAND_LATE"]] - plane[COLS["T_LAND_EARLY"]])
         for i in range(land_window):
-            for j in range(pixel_height): 
-                image.putpixel((int(plane[COLS["T_LAND_EARLY"]]) + i - 1, (idx*row_height)+j), (0,180,0))
+            #Draw top and bottom lines
+            image.putpixel((int(plane[COLS["T_LAND_EARLY"]]) + i - 1, (idx*row_height)+gap_height), (0,0,0))
+            image.putpixel((int(plane[COLS["T_LAND_EARLY"]]) + i - 1, (idx*row_height)+row_height-gap_height-1), (0,0,0))
 
-        for j in range(pixel_height):
-            image.putpixel((int(plane[COLS["T_LAND_ASSIGNED"]])-1, (idx*row_height)+j), (255,255,255))
+        #Draw left (early) and right (late) lines
+        for j in range(pixel_height-gap_height):
+            image.putpixel((int(plane[COLS["T_LAND_EARLY"]])-1, (idx*row_height)+gap_height+j), (0,0,0))
+            image.putpixel((int(plane[COLS["T_LAND_LATE"]])-1, (idx*row_height)+gap_height+j), (0,0,0))
 
-        for j in range(pixel_height):
-            image.putpixel((int(plane[COLS["T_APPEAR"]])-1, (idx*row_height)+j), (255,100,100))
+        #Draw appearance time
+        for j in range(pixel_height-gap_height):
+            image.putpixel((int(plane[COLS["T_APPEAR"]])-1, (idx*row_height)+gap_height+j), (0,0,0))
 
-        for j in range(pixel_height):
-            image.putpixel((int(plane[COLS["T_LAND_TARGET"]])-1, (idx*row_height)+j), (255,255,128))
+        #Draw appearance time whisker
+        land_delay = int(plane[COLS["T_LAND_EARLY"]] - plane[COLS["T_APPEAR"]])
+        for i in range(land_delay):
+            image.putpixel((int(plane[COLS["T_APPEAR"]]) + i - 1, (idx*row_height)+int((pixel_height+gap_height)/2)), (0,0,0))
+
+        #Draw assigned time
+        for j in range(pixel_height-gap_height):
+            image.putpixel((int(plane[COLS["T_LAND_ASSIGNED"]])-1, (idx*row_height)+gap_height+j), (0,0,0))
+
+        #Draw target time
+        for j in range(pixel_height-gap_height-1):
+            image.putpixel((int(plane[COLS["T_LAND_TARGET"]])-1, (idx*row_height)+gap_height+j), (0,0,0) if j % 2 == 0 else (255,255,255))
 
     image.show()
     # im.save('simplePixel.png') # or any image format
@@ -89,7 +120,13 @@ class ArchiveCallback(Callback):
 
 problem = PlaneProblem(data.shape[1], lower_bounds, upper_bounds)
 
-# algorithm = NSGA2(pop_size=n_planes)
+algorithm = NSGA2(
+    pop_size=n_planes,
+    n_offsprings=10,
+    sampling=data,
+    )
+
+
 # callback = ArchiveCallback()
 # res = minimize(PlaneProblem(xl, xu),
 #                algorithm,
